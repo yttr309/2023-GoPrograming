851c4111e535aded1d170ad5e1feabaaeff4d4a3
leejungjae <yttr309@naver.com>
2023-09-25 10:47:33 +0900

    week_05_2

--- week05/week05.go
+++ week05/week05.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"time"
+	"fmt"
+	"strings"
 )
 
 func main() {
-	var now time.Time = time.Now()
-	var year int = now.Year()
-	println(year)
-	month := now.Month()
-	println(month)
-	//println(now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second())
+	broken := "cs r?cks~"
+	replaceWords := strings.NewReplacer("?", "o")
+	fixedWords := replaceWords.Replace(broken)
+	fmt.Println(fixedWords)
 }

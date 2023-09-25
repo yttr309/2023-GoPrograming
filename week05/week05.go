979dd11ccab35c10c73be5d32ce590d491975c5a
leejungjae <yttr309@naver.com>
2023-09-25 11:53:10 +0900

    week_05_08

--- week05/week05.go
+++ week05/week05.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -15,7 +17,15 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	inputScore = strings.TrimSpace(inputScore)
+	score, err := strconv.ParseInt(inputScore, 32)
+	if score >= 60 {
+		grade := "A grade"
+	} else {
+		grade := "under A grade"
+	}
 	fmt.Println(inputScore)
+	fmt.Println("You will get ", grade)
 
 	//broken := "cs r?cks~"
 	//replaceWords := strings.NewReplacer("?", "o")

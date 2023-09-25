837b5eb2f5d52be2828fe8c0c933bbdf15734ac8
leejungjae <yttr309@naver.com>
2023-09-25 10:39:29 +0900

    week_05

--- week05/week05.go
+++ week05/week05.go
@@ -8,5 +8,7 @@ func main() {
 	var now time.Time = time.Now()
 	var year int = now.Year()
 	println(year)
-	println(now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second())
+	month := now.Month()
+	println(month)
+	//println(now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second())
 }
